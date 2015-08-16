b7568d414295b02f9d94ecadd1277a734a983fff
ekrishnakumar <e.krishnakumar@gmail.com>
2015-08-16 10:04:47 +0530

    Updated CSS

--- SampleWebApplication/SampleWebApplication/Models/AccountViewModels.cs
+++ SampleWebApplication/SampleWebApplication/Models/AccountViewModels.cs
@@ -70,7 +70,8 @@ namespace SampleWebApplication.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessageResourceType = typeof(AppResource),
+            ErrorMessageResourceName = "MinLengthCheck", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
